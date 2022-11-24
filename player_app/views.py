6519005316c1291d66721a6859b238909d59a334
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-11-24 11:13:06 +0100

    correct age evaluation

--- player_app/views.py
+++ player_app/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
@@ -51,10 +52,31 @@ def player_details_or_update_or_delete(request, pk):
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
     #or
     #return JsonResponse({'message': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED) 
+def age(birthdate):
+    # Get today's date object
+    today = date.today()
+    
+    # A bool that represents if today's day/month precedes the birth day/month
+    one_or_zero = ((today.month, today.day) < (birthdate.month, birthdate.day))
+    
+    # Calculate the difference in years from the date object's components
+    year_difference = today.year - birthdate.year
+    
+    # The difference in years is not enough. 
+    # To get it right, subtract 1 or 0 based on if today precedes the 
+    # birthdate's month/day.
+    
+    # To do this, subtract the 'one_or_zero' boolean 
+    # from 'year_difference'. (This converts
+    # True to 1 and False to 0 under the hood.)
+    age = year_difference - one_or_zero
+    
+    return age
+
 @api_view(['GET'])
 def player_age_gte(request, age_seuil):
     try:
-        player = Player.objects.filter(age__gte=age_seuil)
+        player = Player.objects.filter(birthDate__year__gte=date.today().year-age_seuil)
         serializer = PlayerSerializer(player, context={'request': request}, many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
     except Player.DoesNotExist:
